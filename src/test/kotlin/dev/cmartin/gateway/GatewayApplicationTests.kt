1acbd4e0dc0e2c733589d47e595cda660491aee2
Carlos Martin <cmarting@gmail.com>
2026-06-20 20:06:05 +0200

    fix obsolete config

--- src/test/kotlin/dev/cmartin/gateway/GatewayApplicationTests.kt
+++ src/test/kotlin/dev/cmartin/gateway/GatewayApplicationTests.kt
@@ -1,13 +1,20 @@
 package dev.cmartin.gateway
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.gateway.server.mvc.config.GatewayMvcProperties
 
 @SpringBootTest
 class GatewayApplicationTests {
 
+	@Autowired
+	private lateinit var gatewayProperties: GatewayMvcProperties
+
 	@Test
 	fun contextLoads() {
+		assertThat(gatewayProperties.routes.map { it.id }).containsExactly("ms-one", "ms-two")
 	}
 
 }
